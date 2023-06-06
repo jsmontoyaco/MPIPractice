761596f17ab2573d9847a9e99c1fd5d628f6402a
Juan Sebastian Montoya Combita <jsmontoyaco@unal.edu.co>
2023-06-06 14:26:09 -0500

    Matrix mul finished and working
    Matrix mul on mpi finished and working locally

--- matrixmulmpi.c
+++ matrixmulmpi.c
@@ -6,14 +6,14 @@
 
 #define N 4
 
-void printmatrix(double *matrix, char ptext[])
+void printmatrix(double *matrix, int rows, int columns, char ptext[])
 {
     printf("%s\n", ptext);
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < columns; j++)
         {
-            printf("%.2f\t", matrix[i*N +j]);
+            printf("%.2f\t", matrix[i*columns +j]);
         }
         printf("\n");
     }
@@ -24,80 +24,86 @@ int main(int argc, char **argv)
     int rank, size, begin, end;
     clock_t startt, endt;
     double totalTime;
-
-    // time init
+    double *a, *b, *r, *a_recv, *r_send;
+    
+    // Time start
     startt = clock();
-
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+    
+    // Size verification
     if (N % size != 0)
     {
-        printf("Error, la matriz no puede dividirse por el número de procesos, procure que sea par e inferior a:%d", N);
+        printf("Error, la matriz no puede dividirse por el número de procesos, procure que sea par e inferior a: %d", N);
         MPI_Finalize();
         exit(-1);
     }
+    
+    // Delimiters
     begin = rank * N / size;
     end = (rank + 1) * N / size;
-
-    double *a = NULL;
-    double *b = NULL;
-    double *r = NULL;
-
+    
+    // Matrix pieces and b
+    a_recv = (double *)malloc(N * (N / size) * sizeof(double));
+    r_send = (double *)malloc(N * (N / size) * sizeof(double));
+    b = (double *)malloc(N * N * sizeof(double));
+    
+    // Original matrices
     if (rank == 0)
     {
-        // Create sub matrixes using pointers
         a = (double *)malloc(N * N * sizeof(double));
-        b = (double *)malloc(N * N * sizeof(double));
         r = (double *)malloc(N * N * sizeof(double));
-    
-
-        //fill matrixes with random values
+        
         for (int i = 0; i < N*N; i++)
         {
             a[i] = 2.0 * ((double)rand() / RAND_MAX) - 1.0;
             b[i] = 2.0 * ((double)rand() / RAND_MAX) - 1.0;
-            
         }
     }
-    // Scatter to distribute all values of matrix a in ranges
-    MPI_Scatter(a, N * N / size, MPI_DOUBLE, a+begin*N, N * N / size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    // Broadcast to assign b matrix to every process
-    MPI_Bcast(b, N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    // Operation for every submatrix
+    
+    // Distribute the matrix
+    MPI_Scatter(a, N*N/size, MPI_DOUBLE, a_recv, N*N/size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    // Copy b to every process
+    MPI_Bcast(b, N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    // Matrix multiplication by row    
     for (int i = begin; i < end; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            r[(i-begin)*N+j] = 0.0;
+            r_send[(i-begin)*N + j] = 0.0;
             for (int k = 0; k < N; k++)
             {
-                r[(i-begin)*N+j] += a[(i-begin)*N+k] * b[k*N+j];
+                r_send[(i-begin)*N + j] += a_recv[(i-begin)*N + k] * b[j + k*N];
             }
         }
     }
-
-    // Recollect data from processes and get result on R matrix
-
-    MPI_Gather(r, N * N / size, MPI_DOUBLE, r, N * N / size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
+    
+    // Recollect results in r matrix
+    MPI_Gather(r_send, N * N/size, MPI_DOUBLE, r, N * N/size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    // Finish
     if (rank == 0)
     {
-        printmatrix(a, "matriz a:");
-        printmatrix(b, "matriz b:");
-        printmatrix(r, "Resultado");
-        // Finish size time
+        // Time end
         endt = clock();
         totalTime = ((double)(endt - startt)) / CLOCKS_PER_SEC;
         printf("Fin del programa\nTiempo de ejecución:\n%f\n", totalTime);
+        
+        printmatrix(a, N, N, "Matriz a:");
+        printmatrix(b, N, N, "Matriz b:");
+        printmatrix(r, N, N, "Resultado:");
+        free(a);
+        free(r);
     }
-
-    free(a);
+    
+    free(a_recv);
     free(b);
-    free(r);
-
+    free(r_send);
+    
     MPI_Finalize();
     
     return 0;
